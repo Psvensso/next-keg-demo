ba593a8778de6aca1725347d1d10898031ba66b1
PEsven <per.svensson.se@gmail.com>
2025-05-01 09:28:44 +0200

    seq seed script to not overwhelm the connection

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@/db/generated";
 import csv from "csv-parser";
+import * as dotenv from "dotenv";
 import * as fs from "fs";
 import * as path from "path";
 import stripBom from "strip-bom-stream";
 import { fileURLToPath } from "url";
+
+dotenv.config({ override: true });
 
 //Makes a url safe slug name from a course title
 function sluggify(str: string) {
@@ -24,10 +27,67 @@ const prisma = new PrismaClient();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+type CourseCsvRow = {
+  CourseId: string;
+  InstituteName: string;
+  CourseName: string;
+  Category: string;
+  DeliveryMethod: string;
+  Location: string;
+  Language: string;
+  StartDate: string;
+};
+
+async function saveRow(row: CourseCsvRow) {
+  try {
+    const instituteNameSlug = sluggify(row.InstituteName);
+    const courseNameSlug = sluggify(row.CourseName);
+    //Just a little seed test
+    if (encodeURIComponent(instituteNameSlug) !== instituteNameSlug) {
+      throw "bad seed, instituteNameSlug: " + instituteNameSlug;
+    }
+    if (encodeURIComponent(courseNameSlug) !== courseNameSlug) {
+      throw "bad seed, courseNameSlug: " + courseNameSlug;
+    }
+
+    await prisma.course.upsert({
+      create: {
+        id: row.CourseId,
+        instituteName: row.InstituteName,
+        instituteNameSlug,
+        courseName: row.CourseName,
+        courseNameSlug,
+        category: row.Category,
+        deliveryMethod: row.DeliveryMethod,
+        location: row.Location,
+        language: row.Language === "NULL" ? null : row.Language,
+        startDate: new Date(row.StartDate),
+      },
+      update: {
+        instituteName: row.InstituteName,
+        instituteNameSlug,
+        courseName: row.CourseName,
+        courseNameSlug,
+        category: row.Category,
+        deliveryMethod: row.DeliveryMethod,
+        location: row.Location,
+        language: row.Language === "NULL" ? null : row.Language,
+        startDate: new Date(row.StartDate),
+      },
+      where: {
+        id: row.CourseId,
+      },
+    });
+  } catch (error) {
+    console.error(`Error processing course ${row.CourseName}:`, error);
+  }
+}
+
 async function seedDatabase() {
   const filePath = path.join(__dirname, "technical_assignment_input_data.csv");
 
-  return new Promise((resolve, reject) => {
+  const rows: CourseCsvRow[] = [];
+  await new Promise((resolve, reject) => {
     fs.createReadStream(filePath)
       .pipe(stripBom())
       .pipe(
@@ -35,36 +95,8 @@ async function seedDatabase() {
           separator: ";",
         })
       )
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .on("data", async (row: any) => {
-        try {
-          const instituteNameSlug = sluggify(row.InstituteName);
-          const courseNameSlug = sluggify(row.CourseName);
-          //Just a little seed test
-          if (encodeURIComponent(instituteNameSlug) !== instituteNameSlug) {
-            throw "bad seed, instituteNameSlug: " + instituteNameSlug;
-          }
-          if (encodeURIComponent(courseNameSlug) !== courseNameSlug) {
-            throw "bad seed, courseNameSlug: " + courseNameSlug;
-          }
-
-          await prisma.course.create({
-            data: {
-              id: row.CourseId,
-              instituteName: row.InstituteName,
-              instituteNameSlug,
-              courseName: row.CourseName,
-              courseNameSlug,
-              category: row.Category,
-              deliveryMethod: row.DeliveryMethod,
-              location: row.Location,
-              language: row.Language === "NULL" ? null : row.Language,
-              startDate: new Date(row.StartDate),
-            },
-          });
-        } catch (error) {
-          console.error(`Error processing course ${row.CourseName}:`, error);
-        }
+      .on("data", (row) => {
+        rows.push(row);
       })
       .on("end", async () => {
         console.log(`Database seeding completed`);
@@ -76,6 +108,12 @@ async function seedDatabase() {
         reject(error);
       });
   });
+
+  for (const row of rows) {
+    await saveRow(row);
+  }
+
+  return;
 }
 
 seedDatabase().catch((e) => {
