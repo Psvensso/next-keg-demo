055a9c1dc0f5dbe2ea3ead52d38a88f63b415137
PEsven <per.svensson.se@gmail.com>
2025-05-01 10:33:52 +0200

    refactor: revert back to custom env strings (deployment fixes)

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -1,16 +1,31 @@
 import * as dotenv from "dotenv";
 import { PrismaClient } from "./generated";
+
 const environment = process.env.NODE_ENV || "development";
-const envFileName = `.env.${environment}`;
-dotenv.config({ path: envFileName });
-console.log(envFileName);
-const prismaClient = new PrismaClient({
-  log: environment === "development" ? ["warn", "error"] : [],
-});
+dotenv.config({ path: ".env", override: true });
 
-const globalForPrisma = global as unknown as { prisma: typeof prismaClient };
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prismaClient;
+// Declare global type for Prisma
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+// Use existing client if it exists or create a new one
+const prismaClient =
+  global.prisma ||
+  new PrismaClient({
+    log: environment === "development" ? ["warn", "error"] : [],
+  });
+
+// Assign client to global object to reuse connection between hot reloads
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prismaClient;
+}
 
 export default prismaClient;
